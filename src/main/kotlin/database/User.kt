c90e34eb712a721e2e924944650276ac658066cb
frequensy <mametevalex@gmail.com>
2023-03-30 18:01:02 +0300

    Add targetlang property to database

--- src/main/kotlin/database/User.kt
+++ src/main/kotlin/database/User.kt
@@ -4,16 +4,20 @@ import org.ktorm.database.Database
 import org.ktorm.entity.Entity
 import org.ktorm.entity.sequenceOf
 import org.ktorm.schema.*
+import reverso.LanguageCode
 
 object Users : Table<User>("t_users") {
     val id = int("id").primaryKey().bindTo { it.id }
     val name = varchar("name").bindTo { it.name }
     val chatId = varchar("chat_id").bindTo { it.chatId }
+    val targetLanguage = varchar("target_language").bindTo { it.targetLanguage }
 }
 
 interface User : Entity<User> {
     val id: Int
     val name: String
+    val targetLanguage: String
+    val targetLanguageCode get() = LanguageCode(targetLanguage)
     val chatId: String
 }
 

287fdc7f676b73f726cbb2e20d53e819866d0e0f
RudenkoAD <rudenkoAD@phystech.edu>
2023-03-30 18:30:37 +0300

    [UPD] removed the id attribute from database

--- src/main/kotlin/database/User.kt
+++ src/main/kotlin/database/User.kt
@@ -3,20 +3,19 @@ package database
 import org.ktorm.database.Database
 import org.ktorm.entity.Entity
 import org.ktorm.entity.sequenceOf
-import org.ktorm.schema.*
+import org.ktorm.schema.Table
+import org.ktorm.schema.varchar
 import reverso.LanguageCode
 
 object Users : Table<User>("t_users") {
-    val id = int("id").primaryKey().bindTo { it.id }
     val name = varchar("name").bindTo { it.name }
-    val chatId = varchar("chat_id").bindTo { it.chatId }
+    val chatId = varchar("chat_id").primaryKey().bindTo { it.chatId }
     val targetLanguage = varchar("target_language").bindTo { it.targetLanguage }
 }
 
 interface User : Entity<User> {
-    val id: Int
     val name: String
-    val targetLanguage: String
+    var targetLanguage: String
     val targetLanguageCode get() = LanguageCode(targetLanguage)
     val chatId: String
 }

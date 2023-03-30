c7d5cfc64026fa5d6cc290c804d93122deb26044
RudenkoAD <rudenkoAD@phystech.edu>
2023-03-30 19:39:01 +0300

    [FIX] added filters to behaviorbuilder expectations [UPD] added flags to KeyboardsManager

--- src/main/kotlin/KeyboardsManager.kt
+++ src/main/kotlin/KeyboardsManager.kt
@@ -16,9 +16,11 @@ class KeyboardsManager {
     }
     fun getLanguageChoiceKeyboard() =
         inlineKeyboard {
-            row{dataButton(text = "Russian", data = "ChangelangTo_ru")}
-            row{dataButton(text = "English", data = "ChangelangTo_en")}
-            row{dataButton(text = "French", data = "ChangelangTo_fr")}
-            row{dataButton(text = "Chinese", data = "ChangelangTo_ch")}
+            //language codes are [ru, en, de, ar, es, fr, he, it, ja, ko, nl, pl, pt, ro, sv, tr, zh, uk]
+            row{dataButton(text = "Russian \uD83C\uDDF7\uD83C\uDDFA", data = "ChangelangTo_ru")}
+            row{dataButton(text = "English \uD83C\uDDFA\uD83C\uDDF8", data = "ChangelangTo_en")}
+            row{dataButton(text = "German \uD83C\uDDE9\uD83C\uDDEA", data = "ChangelangTo_de")}
+            row{dataButton(text = "French \uD83C\uDDEB\uD83C\uDDF7", data = "ChangelangTo_fr")}
+            row{dataButton(text = "Chinese \uD83C\uDDE8\uD83C\uDDF3", data = "ChangelangTo_zh")}
         }
 }
